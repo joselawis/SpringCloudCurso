8d516671aa6931f0f40f212972e4a974a5e98f2a
joselawis <joselawis@gmail.com>
2025-05-27 14:32:16 +0200

    added sample cookie filter for products

---
+++ msvc-gateway-server/src/main/java/com/lawis/springcloud/app/gateway/filters/factory/SampleCookieGatewayFilterFactory.java
@@ -0,0 +1,49 @@
+package com.lawis.springcloud.app.gateway.filters.factory;
+
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+import reactor.core.publisher.Mono;
+
+@Component
+public class SampleCookieGatewayFilterFactory
+        extends AbstractGatewayFilterFactory<SampleCookieGatewayFilterFactory.ConfigurationCookie> {
+
+    private final Logger logger = LoggerFactory.getLogger(SampleCookieGatewayFilterFactory.class);
+
+    public SampleCookieGatewayFilterFactory() {
+        super(ConfigurationCookie.class);
+    }
+
+    @Override
+    public GatewayFilter apply(ConfigurationCookie config) {
+        return (exchange, chain) -> {
+            logger.info("Executing pre gateway filter factory: " + config.message);
+
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                Optional.ofNullable(config.value).ifPresent(cookie -> {
+                    exchange.getResponse()
+                            .addCookie(ResponseCookie.from(config.name, cookie).build());
+                });
+
+                logger.info("Executing post gateway filter factory: " + config.message);
+            }));
+        };
+    }
+
+    @Getter
+    @Setter
+    public static class ConfigurationCookie {
+        private String name;
+        private String value;
+        private String message;
+    }
+}
