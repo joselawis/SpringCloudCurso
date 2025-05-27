62b7368c7ec73342a74099e8820defcc43ba2afb
joselawis <joselawis@gmail.com>
2025-05-27 14:11:15 +0200

    global filter

--- msvc-gateway-server/src/main/java/com/lawis/springcloud/app/gateway/filters/SampleGlobalFilter.java
+++ msvc-gateway-server/src/main/java/com/lawis/springcloud/app/gateway/filters/SampleGlobalFilter.java
@@ -1,18 +1,22 @@
 package com.lawis.springcloud.app.gateway.filters;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseCookie;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 
 import reactor.core.publisher.Mono;
 
 @Component
-public class SampleGlobalFilter implements GlobalFilter {
+public class SampleGlobalFilter implements GlobalFilter, Ordered {
 
     private final Logger logger = LoggerFactory.getLogger(SampleGlobalFilter.class);
 
@@ -21,18 +25,43 @@ public class SampleGlobalFilter implements GlobalFilter {
 
         logger.info("Executing filter before request PRE processing");
 
-        return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+        ServerHttpRequest mutatedRequest = exchange.getRequest()
+                .mutate()
+                .header("token", "abcdefg")
+                .build();
+
+        ServerWebExchange mutatedExchange = exchange.mutate()
+                .request(mutatedRequest)
+                .build();
+
+        return chain.filter(mutatedExchange).then(Mono.fromRunnable(() -> {
             logger.info("Executing POST filter response");
+
+            String token = mutatedExchange.getRequest()
+                    .getHeaders()
+                    .getFirst("token");
+
+            Optional.ofNullable(token).ifPresent(value -> {
+                logger.info("Token: " + value);
+
+                mutatedExchange.getResponse()
+                        .getHeaders()
+                        .add("token", value);
+            });
 
             exchange.getResponse()
                     .getCookies()
-                    .add("color", ResponseCookie.from("color", "red")
-                            .build());
+                    .add("color", ResponseCookie.from("color", "red").build());
             exchange.getResponse()
                     .getHeaders()
                     .setContentType(MediaType.TEXT_PLAIN);
         }));
 
+    }
+
+    @Override
+    public int getOrder() {
+        return 100;
     }
 
 }
