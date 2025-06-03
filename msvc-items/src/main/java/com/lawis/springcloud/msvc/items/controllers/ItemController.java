9ac5298c3d8fbc0417e5667c0dbf0522cef0a0fc
joselawis <joselawis@gmail.com>
2025-06-03 12:49:31 +0200

    CircuitBreaker and TimeLimiter annotations

--- msvc-items/src/main/java/com/lawis/springcloud/msvc/items/controllers/ItemController.java
+++ msvc-items/src/main/java/com/lawis/springcloud/msvc/items/controllers/ItemController.java
@@ -6,10 +6,16 @@ import com.lawis.springcloud.msvc.items.models.Item;
 import com.lawis.springcloud.msvc.items.models.Product;
 import com.lawis.springcloud.msvc.items.services.ItemService;
 
+import feign.Response;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
+
 import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -64,4 +70,53 @@ public class ItemController {
                 .body(Collections.singletonMap("message", "Product doesn't exist in msvc-product"));
     }
 
+    @CircuitBreaker(name = "items", fallbackMethod = "getFallbackMethodProduct")
+    @GetMapping("/details2/{id}")
+    public ResponseEntity<?> details2(@PathVariable Long id) {
+        Optional<Item> itemOptional = service.findById(id);
+        if (itemOptional.isPresent()) {
+            return ResponseEntity.ok(itemOptional.orElseThrow());
+        }
+        return ResponseEntity
+                .status(404)
+                .body(Collections.singletonMap("message", "Product doesn't exist in msvc-product"));
+    }
+
+    @TimeLimiter(name = "items", fallbackMethod = "getFallbackMethodProduct2")
+    @GetMapping("/details3/{id}")
+    public CompletableFuture<?> details3(@PathVariable Long id) {
+        return CompletableFuture.supplyAsync(() -> {
+            Optional<Item> itemOptional = service.findById(id);
+            if (itemOptional.isPresent()) {
+                return ResponseEntity.ok(itemOptional.orElseThrow());
+            }
+            return ResponseEntity
+                    .status(404)
+                    .body(Collections.singletonMap("message", "Product doesn't exist in msvc-product"));
+        });
+    }
+
+    @SuppressWarnings("unused")
+    private ResponseEntity<?> getFallbackMethodProduct(Throwable e) {
+        logger.error(e.getMessage());
+        Product product = new Product();
+        product.setId(1L);
+        product.setName("Default Product");
+        product.setPrice(100.0);
+        product.setCreatedAt(LocalDate.now());
+        return ResponseEntity.ok(new Item(product, 5));
+    }
+
+    @SuppressWarnings("unused")
+    private CompletableFuture<?> getFallbackMethodProduct2(Throwable e) {
+        return CompletableFuture.supplyAsync(() -> {
+            logger.error(e.getMessage());
+            Product product = new Product();
+            product.setId(1L);
+            product.setName("Default Product");
+            product.setPrice(100.0);
+            product.setCreatedAt(LocalDate.now());
+            return ResponseEntity.ok(new Item(product, 5));
+        });
+    }
 }
