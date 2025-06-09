0fa981d934b69407b71c0ba7cbf739ee350b1f46
joselawis <joselawis@gmail.com>
2025-06-09 13:39:23 +0200

    more configurations

--- msvc-gateway-server/src/main/java/com/lawis/springcloud/app/gateway/security/SecurityConfig.java
+++ msvc-gateway-server/src/main/java/com/lawis/springcloud/app/gateway/security/SecurityConfig.java
@@ -7,9 +7,13 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity.CorsSpec;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
 
 @Configuration
 public class SecurityConfig {
+
+    private static final String ROLE_USER = "USER";
+    private static final String ROLE_ADMIN = "ADMIN";
 
     @Bean
     SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) throws Exception {
@@ -17,13 +21,14 @@ public class SecurityConfig {
                 .pathMatchers("/authorized", "/logout").permitAll()
                 .pathMatchers(HttpMethod.GET, "/api/items", "/api/products", "/api/users").permitAll()
                 .pathMatchers(HttpMethod.GET, "/api/items/{id}", "/api/products/{id}", "/api/users/{id}")
-                .hasAnyRole("ADMIN", "USER")
-                .pathMatchers("/api/items/**", "/api/products/**", "/api/users/**").hasRole("ADMIN")
+                .hasAnyRole(ROLE_ADMIN, ROLE_USER)
+                .pathMatchers("/api/items/**", "/api/products/**", "/api/users/**").hasRole(ROLE_ADMIN)
                 .anyExchange().authenticated())
                 .cors(CorsSpec::disable)
+                .securityContextRepository(NoOpServerSecurityContextRepository.getInstance())
                 .oauth2Login(Customizer.withDefaults())
                 .oauth2Client(Customizer.withDefaults())
-                .oauth2ResourceServer(Customizer.withDefaults())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
     }
 }
