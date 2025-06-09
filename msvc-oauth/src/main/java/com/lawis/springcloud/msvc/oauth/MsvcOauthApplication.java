34a581e1f3f28afc1bb4ecdda2b9a57d0c44defd
joselawis <joselawis@gmail.com>
2025-06-09 11:38:43 +0200

    package correction

--- msvc-oauth/src/main/java/com/lawis/springcloud/msvc/oauth/MsvcOauthApplication.java
+++ msvc-oauth/src/main/java/com/lawis/springcloud/msvc/oauth/MsvcOauthApplication.java
@@ -1,4 +1,4 @@
-package com.lawis.springcloud.msvc.oauth.msvc_oauth;
+package com.lawis.springcloud.msvc.oauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
