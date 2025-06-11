3d8b6fbeda569626554273a045027ee3c87c7741
joselawis <joselawis@gmail.com>
2025-06-11 14:12:09 +0200

    gateway tests

--- msvc-gateway-server/src/test/java/com/lawis/springcloud/app/gateway/msvc_gateway_server/MsvcGatewayServerApplicationTests.java
+++ msvc-gateway-server/src/test/java/com/lawis/springcloud/app/gateway/msvc_gateway_server/MsvcGatewayServerApplicationTests.java
@@ -1,11 +1,7 @@
 package com.lawis.springcloud.app.gateway.msvc_gateway_server;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 
-@SpringBootTest
-@Import(TestSecurityConfig.class)
 class MsvcGatewayServerApplicationTests {
 
 	@Test
