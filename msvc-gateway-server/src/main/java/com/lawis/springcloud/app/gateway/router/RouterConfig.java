3fec385379db88f8984d28355c523d7422a1d8a4
joselawis <joselawis@gmail.com>
2025-06-12 12:59:54 +0200

    added request and response modification filter on product

--- msvc-gateway-server/src/main/java/com/lawis/springcloud/app/gateway/router/RouterConfig.java
+++ msvc-gateway-server/src/main/java/com/lawis/springcloud/app/gateway/router/RouterConfig.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerRequest;
 import org.springframework.web.servlet.function.ServerResponse;
 
 @Configuration
@@ -21,6 +22,15 @@ public class RouterConfig {
     RouterFunction<ServerResponse> router() {
         return route("msvc-products")
                 .route(path("/api/products/**"), http())
+                .filter((request, next) -> {
+                    ServerRequest requestModified = ServerRequest.from(request)
+                            .header("message-request", "Message from msvc-gateway-server to msvc-products")
+                            .build();
+                    ServerResponse response = next.handle(requestModified);
+                    response.headers().add("message-response",
+                            "Message from msvc-gateway-server (and msvc-products) to client");
+                    return response;
+                })
                 .filter(lb("msvc-products"))
                 .filter(circuitBreaker(config -> config.setId("products")
                         .setStatusCodes(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
